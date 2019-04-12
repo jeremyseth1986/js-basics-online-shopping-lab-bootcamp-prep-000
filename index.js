dd31c0858cb6234c0527bbb4f70c6226a07d0682
Jeremy Colon Arroyo <jeremy.seth19@gmail.com>
2019-04-12 23:35:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,12 +10,39 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+var price = Math.floor(Math.random() * 101);
+cart.push(new Object({
+  "itemName" : item,
+  "itemPrice": price
+}));
+if(cart === undefined){
+  return "Your shopping cart is empty."
+  } else {
+  return `${item} has been added to your cart.`
+  
+  }
 }
 
-function viewCart() {
-  // write your code here
+
+
+function viewCart(){
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  } else if (cart.length === 1) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  } else if (cart.length === 2) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+  } else {
+    var myString = "In your cart, you have "
+    for(var i = 0; i < cart.length - 1; i++) {
+      myString = myString + `${cart[i].itemName} at $${cart[i].itemPrice}, `
+    }
+    return myString + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
+  }
 }
+
+  
+
 
 function total() {
   // write your code here
