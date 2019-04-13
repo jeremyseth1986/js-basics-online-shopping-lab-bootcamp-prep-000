4df2a0e457d48c88a8393005dbb2fff6b549b6c9
Jeremy Colon Arroyo <jeremy.seth19@gmail.com>
2019-04-13 03:11:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,5 +66,13 @@ function removeFromCart(item) {
 
 
 
-function placeOrder(cardNumber) 
+function placeOrder(cardNumber)  {
+ if (typeof(cardNumber) === "undefined"){
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    cart = [];
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+  }
+ 
+}
 
