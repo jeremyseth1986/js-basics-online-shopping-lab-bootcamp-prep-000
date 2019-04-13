9436a1479d926dce101e25bfdeee1504eb381c13
Jeremy Colon Arroyo <jeremy.seth19@gmail.com>
2019-04-13 03:57:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,9 +71,9 @@ function placeOrder(cardNumber)  {
  if (typeof(cardNumber) === "undefined"){
     console.log("Sorry, we don't have a credit card on file for you.")
   } else {
+     cart = []
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
- cart = []
- return cart
+
   }
 }
 
