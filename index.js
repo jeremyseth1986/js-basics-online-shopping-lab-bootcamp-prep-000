a51c3b1df9b7b3381214347cb3d4e152926c5e8a
Jeremy Colon Arroyo <jeremy.seth19@gmail.com>
2019-04-13 03:47:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function viewCart(){
 function total(){
  let total = 0;
   for(var i = 0; i < cart.length; i++) {
-    total += cart[i].price;
+    total += cart[i].itemPrice;
   }
   return total;
 }
