b7e1a32715d934d2e76c1e3c6d81797db166686d
Jeremy Colon Arroyo <jeremy.seth19@gmail.com>
2019-04-13 03:51:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function placeOrder(cardNumber)  {
     return "Sorry, we don't have a credit card on file for you."
   } else {
     cart = []
-    var randomPrice = Math.floor(Math.random() * 101);
+   
     return `Your total cost is $${randomPrice}, which will be charged to the card ${cardNumber}.`
   }
  
