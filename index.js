842be36e73d4df88997e9ea05ab31c57bcf65b74
Jeremy Colon Arroyo <jeremy.seth19@gmail.com>
2019-04-13 03:33:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function placeOrder(cardNumber)  {
     return "Sorry, we don't have a credit card on file for you."
   } else {
     cart = []
-    return `Your total cost is $${Total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
  
 }
